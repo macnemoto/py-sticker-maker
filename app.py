2603ec6e0c90b0d243ef960ee3a2b6ed98297067
macnemoto <marcosj465@gmail.com>
2024-07-16 12:12:53 -0400

    first commit py-sticker-maker

---
+++ app.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import pandas as pd
+
+# Convertidor de .wedp a Gif
+
+# im = Image.open('4x668915fcfbe3f7b7cf0940cc.webp')
+
+# im.save('4x668915fcfbe3f7b7cf0940cc.gif', 'gif', save_all=True)
+
+
+
+# Modoficar link -----------------------------
+
+# stickers_file = 'stickers.csv'
+
+# df = pd.read_csv(stickers_file)
+
+# links_search = []
+
+# links = df['Links'].tolist()
+
+# for link in links:
+#     original_link = link
+#     cdn_domain = 'cdn.7tv.app/emote'
+
+#     modifications_link = original_link.replace('https://7tv.app/emotes', f'https://{cdn_domain}')
+#     modifications = f'{modifications_link}/4x.webp'
+#     links_search.append(modifications)
+
+# print(links_search)
+
+# Modoficar link -----------------------------
